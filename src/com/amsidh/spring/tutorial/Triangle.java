3512ea54dac8cf5bbabf2007d1c15f7aed0ce4b3
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-19 22:42:54 +0530

    Initial Commit

---
+++ src/com/amsidh/spring/tutorial/Triangle.java
@@ -0,0 +1,49 @@
+package com.amsidh.spring.tutorial;
+
+
+public class Triangle {
+
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+    	
+
+	public Triangle(Point pointA, Point pointB, Point pointC) {
+		super();
+		this.pointA = pointA;
+		this.pointB = pointB;
+		this.pointC = pointC;
+	}
+
+
+
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+
+	public void draw()
+	{
+		
+			System.out.println("PointA ("+pointA.getX()+","+pointA.getY()+")");
+			System.out.println("PointB ("+pointB.getX()+","+pointB.getY()+")");
+			System.out.println("PointC ("+pointC.getX()+","+pointC.getY()+")");
+		
+	}
+}
